e901be1a3fafacee1fc2ce8e86678af29c320f94
olesgedz <olesgedz@rambler.ru>
2020-02-01 13:17:48 +0300

    can rotate planet

---
+++ Assets/Scenes/Oles/Scripts/MouseRotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseRotate : MonoBehaviour
+{
+   [SerializeField] float rotationSpeed = 0.2f;
+
+    void OnMouseDrag()
+    {
+        float XaxisRotation = Input.GetAxis("Mouse X") * rotationSpeed;
+        float YaxisRotation = Input.GetAxis("Mouse Y") * rotationSpeed;
+        transform.RotateAround(Vector3.down, XaxisRotation);
+        transform.RotateAround(Vector3.right, YaxisRotation);
+    }
+}

24e89d91dc5454e8b4cb3817e46cb5a2e72b71c3
TebaHa <Tchurlyaev.teba@yandex.com>
2020-02-01 21:39:25 +0300

    eto zhe commit

---
+++ Assets/Scripts/HexController/HexData.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum HexState {Dead, Alive};
+public enum DisasterState {None, Dry, Fire};
+public enum HexProgress {Nothing = 0, Birth = 1 , StableBirth = 2 , Animals = 3, StableAnimals = 4, Tribe = 5, Village = 6, SmallCity = 7, Megapolice = 8, Winner = 9};
+
+public class HexData : MonoBehaviour
+{
+    List<HexData> hexNeibours = new List<HexData>();
+    public HexState hexStatusState = HexState.Dead;
+    public DisasterState disasterState;
+    public HexProgress hexProgressState;
+
+        MeshRenderer model;
+        float waterBalance;
+        float temperatureBalance;
+        float disasterProgress;
+        float generalProgress;
+        float addition;
+        Color startColorLife;
+        Color endColorLife;
+
+    private void setIdle(HexState newHexState)
+    {
+        this.hexStatusState = newHexState;
+        hexProgressState = HexProgress.Nothing;
+        waterBalance = 0;
+        temperatureBalance = 0;
+        disasterProgress = 0;
+        disasterState = DisasterState.None;
+    }
+
+    private void setIdle()
+    {
+        hexProgressState = HexProgress.Nothing;
+        waterBalance = 0;
+        temperatureBalance = 0;
+        disasterProgress = 0;
+        disasterState = DisasterState.None;
+    }
+
+    private void getNeibours()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(this.transform.position, 1);
+        foreach (var gobject in hitColliders)
+        {
+            if (this.gameObject != gobject)
+            {
+                hexNeibours.Add(gobject.GetComponent<HexData>());
+            }
+        }
+    }
+
+    private void updateColor()
+    {
+        if (generalProgress > 200)
+            generalProgress = 200;
+        model.material.SetColor("_Color", Color32.Lerp(startColorLife, endColorLife, Mathf.PingPong(generalProgress / 200, 1)));
+    }
+
+    public void influeceNeibours()
+    {
+        if (hexProgressState >= HexProgress.StableBirth)
+        {
+            foreach (HexData hex in hexNeibours)
+            {
+                if (hex.isAlive())
+                    hex.updateProgress(generalProgress / 2);
+                else
+                    hex.live();
+            }
+        }
+    }
+
+    public void die()
+    {
+        this.setIdle(HexState.Dead);
+    }
+
+    public void live()
+    {
+        this.setIdle(HexState.Alive);
+        this.hexProgressState = HexProgress.Birth;
+    }
+
+    public HexState getState()
+    {
+        return hexStatusState;
+    }
+
+    public void updateHex()
+    {
+        if (hexStatusState == HexState.Dead)
+        {
+            return ;
+        }
+        if (disasterState == DisasterState.Dry 
+        | disasterState == DisasterState.Fire)
+        {
+            disasterProgress += (float)(2.0 * Time.deltaTime);
+        }
+        if (disasterProgress >= 100)
+        {
+            this.die();
+            return ;
+        }
+        if (temperatureBalance >= 100 |
+        temperatureBalance <= -100)
+        {
+            this.die();
+            return;
+        }
+        if (waterBalance >= 100 |
+        waterBalance <= -100)
+        {
+            this.die();
+            return;
+        }
+        addition = (float)(20 * Time.deltaTime);
+        generalProgress += addition;
+        hexProgressState = (HexProgress)((int)generalProgress / 100);
+    }
+
+    public void updateProgress(float progress)
+    {
+        generalProgress += progress;
+    }
+
+    public void updateTemperature(float temperature)
+    {
+        temperatureBalance += temperature;
+    }
+
+    public void updateWater(float water)
+    {
+        waterBalance += water;
+    }
+
+    public void setDisaster(DisasterState disaster)
+    {
+        this.disasterState = disaster;
+    }
+
+    public bool isAlive()
+    {
+        return hexStatusState == HexState.Alive;
+    }
+
+    void Start()
+    { 
+        this.setIdle();
+        this.getNeibours();
+        startColorLife = new Color32(162, 167, 160, 255);
+        endColorLife = new Color32(165, 209, 67, 255);
+        model = gameObject.GetComponentInParent<MeshRenderer>();
+        model.material.SetColor("_Color", new Color32(162, 167, 160, 255));
+        Debug.Log(model.material.name);
+    }
+    
+    void Update()
+    {
+    }
+};

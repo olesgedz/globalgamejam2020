e4920668280ac671bc87a20583eb639fc7816646
olesgedz <olesgedz@rambler.ru>
2020-02-01 16:47:55 +0300

    niegthbours

---
+++ Assets/Scenes/Oles/Scripts/GetNeighbours.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetNeighbours : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(this.transform.position, 5);
+        int i = 0;
+        while (i < hitColliders.Length)
+        {
+            Debug.Log(hitColliders[i].gameObject.name);
+            i++;
+        }
+
+    }
+}

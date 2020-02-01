bb742c9deeae21cf992c291cd32e75fa7c45b23a
TebaHa <Tchurlyaev.teba@yandex.com>
2020-02-02 01:28:01 +0300

    merged

---
+++ Assets/Scripts/RenderManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void setObjectArray();
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

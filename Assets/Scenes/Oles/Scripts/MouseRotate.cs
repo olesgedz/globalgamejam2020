1fe1c989d1370ce5171e5f19619959a69f5cbb30
olesgedz <olesgedz@rambler.ru>
2020-02-01 13:57:59 +0300

    can rotate planet

--- Assets/Scenes/Oles/Scripts/MouseRotate.cs
+++ Assets/Scenes/Oles/Scripts/MouseRotate.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MouseRotate : MonoBehaviour
 {
-   [SerializeField] float rotationSpeed = 0.2f;
+    [SerializeField] float rotationSpeed = 0.2f;
 
     void OnMouseDrag()
     {

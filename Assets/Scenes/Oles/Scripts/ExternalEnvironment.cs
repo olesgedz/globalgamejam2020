b95cce1e74703ac76c16fd24e2fcc0517b2d6626
olesgedz <olesgedz@rambler.ru>
2020-02-02 01:21:06 +0300

    raycast debug GizmosDraw

--- Assets/Scenes/Oles/Scripts/ExternalEnvironment.cs
+++ Assets/Scenes/Oles/Scripts/ExternalEnvironment.cs
@@ -5,24 +5,49 @@ using UnityEngine;
 public class ExternalEnvironment : MonoBehaviour
 { 
     [SerializeField] GameObject planet;
-
+    private RaycastHit hit;
     void Start()
     {
-        
+        planet = GameObject.FindGameObjectWithTag("Planet");
     }
 
     void Update()
     {
-        GetEffectedTiles();
+        List<GameObject> list = GetEffectedTiles();
+        foreach(var edit in list)
+        {
+            Debug.Log(edit.gameObject.name);
+        }
+
+
     }
 
     public virtual List<GameObject> GetEffectedTiles()
     {
         List<GameObject> list = new List<GameObject>();
-        RaycastHit hit;
-        Vector3 fwd = transform.TransformDirection(this.gameObject.transform.forward);
-        if (Physics.Raycast(transform.position, fwd, out hit))
-            Debug.Log(hit.rigidbody.gameObject.name);
+
+       // Vector3 fwd = transform.TransformDirection(this.gameObject.transform.forward);
+        Vector3 forward = planet.transform.position - this.transform.position;
+        Debug.DrawRay(transform.position, forward, Color.green);
+    
+        if (Physics.Raycast(transform.position, forward, out hit))
+        {
+            Collider[] hitColliders = Physics.OverlapSphere(hit.point, 1);
+            int i = 0;
+            while (i < hitColliders.Length)
+            {
+                if (this.gameObject != hitColliders[i].gameObject)
+                    list.Add(hitColliders[i].gameObject);
+                i++;
+            }
+        }
         return list;
+    }
+
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(hit.point, 1);
     }
 }

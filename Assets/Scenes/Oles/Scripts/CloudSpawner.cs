60ffd5d39ea19c04d34345161c68f371e9b68e3b
olesgedz <olesgedz@rambler.ru>
2020-02-01 19:13:03 +0300

    clouds

---
+++ Assets/Scenes/Oles/Scripts/CloudSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject cloud;
+
+    void Start()
+    {
+        StartCoroutine(SpawnCloud());
+    }
+
+    void Update()
+    {
+        
+    }
+
+   IEnumerator SpawnCloud()
+    {
+        Vector3 pos = Random.onUnitSphere * 5;
+        GameObject parent = GameObject.FindWithTag("Planet");
+
+        Vector3 targetDir = pos - parent.transform.position;
+        float angle = Vector3.Angle(targetDir, parent.transform.forward);
+
+        GameObject newCloud = Instantiate(cloud, pos, Quaternion.Euler(angle, angle, angle)) as GameObject;
+       
+        newCloud.transform.parent = parent.transform;
+        yield return new WaitForSeconds(2f);
+        StartCoroutine(SpawnCloud());
+    }
+}

9283af914a32927cab50933d5b84525948846b9e
olesgedz <olesgedz@rambler.ru>
2020-02-01 21:40:50 +0300

    started raycasting

--- Assets/Scenes/Oles/Scripts/CloudSpawner.cs
+++ Assets/Scenes/Oles/Scripts/CloudSpawner.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class CloudSpawner : MonoBehaviour
 {
     [SerializeField] GameObject cloud;
-
+    private GameObject parent;
     void Start()
     {
+        parent = GameObject.FindWithTag("Planet");
         StartCoroutine(SpawnCloud());
     }
 
@@ -18,16 +19,34 @@ public class CloudSpawner : MonoBehaviour
 
    IEnumerator SpawnCloud()
     {
-        Vector3 pos = Random.onUnitSphere * 5;
-        GameObject parent = GameObject.FindWithTag("Planet");
+        //Vector3 pos = Random.onUnitSphere * 10;
+        //Debug.Log(pos);
 
-        Vector3 targetDir = pos - parent.transform.position;
-        float angle = Vector3.Angle(targetDir, parent.transform.forward);
 
-        GameObject newCloud = Instantiate(cloud, pos, Quaternion.Euler(angle, angle, angle)) as GameObject;
-       
-        newCloud.transform.parent = parent.transform;
+        //Vector3 targetDir = parent.transform.position - pos;
+        //float angleX = 360 - Vector3.Angle(targetDir, parent.transform.right);
+        //float angleY = 360 - Vector3.Angle(targetDir, parent.transform.up);
+        //float angleZ = 360 - Vector3.Angle(targetDir, parent.transform.forward);
+        //GameObject newCloud = Instantiate(cloud, pos + parent.transform.position, Quaternion.Euler(angleX, angleY, 0)) as GameObject;
+
+
+        Vector3 pos = RandomCircle(parent.transform.position, 20f);
+        Quaternion rot = Quaternion.FromToRotation(cloud.transform.right, parent.transform.position - pos);
+        GameObject newCloud =  Instantiate(cloud, pos, rot) as GameObject;
+        //newCloud.transform.LookAt(newCloud.transform, parent.transform.position);
+        newCloud.transform.SetParent(parent.transform);
+
         yield return new WaitForSeconds(2f);
         StartCoroutine(SpawnCloud());
+    }
+
+    Vector3 RandomCircle(Vector3 center, float radius)
+    {
+        float ang = Random.value * 360;
+        Vector3 pos;
+        pos.x = center.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
+        pos.y = center.y + radius * Mathf.Cos(ang * Mathf.Deg2Rad);
+        pos.z = center.z;
+        return pos;
     }
 }
---
+++ Assets/Scenes/Oles/Scripts/ExternalEnvironment.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExternalEnvironment : MonoBehaviour
+{ 
+    [SerializeField] GameObject planet;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        GetEffectedTiles();
+    }
+
+    public virtual List<GameObject> GetEffectedTiles()
+    {
+        List<GameObject> list = new List<GameObject>();
+        //RaycastHit hit;
+        //Vector3 fwd = transform.TransformDirection(this.gameObject.transform.forward);
+        //if (Physics.Raycast(transform.position, fwd, out hit))
+            //Debug.Log(hit.rigidbody.gameObject.name);
+        return list;
+    }
+}

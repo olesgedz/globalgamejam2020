ca4d8058e54cccc324ffd5d721e16060aa5f20ff
olesgedz <olesgedz@rambler.ru>
2020-02-02 07:54:29 +0300

    07:54

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    [SerializeField] Vector3 Axis;
+    [SerializeField] float RotationSpeed;
+    // Start is called before the first frame update
+
+    private void OnEnable()
+    {
+        if (Axis == Vector3.zero)
+        {
+            Axis = transform.up;
+        }
+        else
+        {
+            Axis = Axis.normalized;
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Axis, RotationSpeed * Time.deltaTime);
+    }
+}

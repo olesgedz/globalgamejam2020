f009d4a3d8070b431c4b5487de1a8c16f8e4c865
olesgedz <olesgedz@rambler.ru>
2020-02-02 06:44:49 +0300

    rot

--- Assets/Scripts/MouseRotate.cs
+++ Assets/Scripts/MouseRotate.cs
@@ -8,9 +8,12 @@ public class MouseRotate : MonoBehaviour
 
     void OnMouseDrag()
     {
+        Debug.Log("fd");
         float XaxisRotation = Input.GetAxis("Mouse X") * rotationSpeed;
         float YaxisRotation = Input.GetAxis("Mouse Y") * rotationSpeed;
         transform.RotateAround(Vector3.down, XaxisRotation);
+        transform.Rotate(Vector3.down, XaxisRotation);
+
         transform.RotateAround(Vector3.right, YaxisRotation);
     }
 }
